60a8e0cea530eec06501d2dc7d4971e195a483eb
Awolad Hossain <awolad1122@gmail.com>
2018-11-11 09:45:36 +0600

    promise

---
+++ playground/promise.js
@@ -0,0 +1,30 @@
+var asyncAdd = (a, b) => {
+  return new Promise((reslove, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        reslove(a + b);
+      } else {
+        reject('Both parameter should be number');
+      }
+    }, 3000);
+  });
+}
+
+asyncAdd(3, '9').then((res) => {
+  console.log(res);
+}, (errorMessage) => {
+  console.log(errorMessage);
+});
+
+// var somePromise = new Promise((reslove, reject) => {
+//   setTimeout(() => {
+//     reslove('Hey, it works!');
+//     reject('Unable to fullfil the promise');
+//   }, 3000);
+// });
+//
+// somePromise.then((message) => {
+//   console.log('Success: ', message);
+// }, (errorMessage) => {
+//   console.log(errorMessage);
+// });
